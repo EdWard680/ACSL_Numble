635ede942fd9b936dac4623e751fe2d90a87231f
EdWard680 <ezeward4@gmail.com>
2014-04-03 17:21:10 -0400

    Reformats with correct tabbing

--- Numble.cpp
+++ Numble.cpp
@@ -2,37 +2,37 @@
 
 NumbleWord::NumbleWord(const string &s, const int indexOfPivot): vector<int>(s.size())  // error checking left to callers
 {
-	for(auto c = s.begin(); c != s.end(); c++) // omg c++
-		push_back(*c);
-	
-	pivot = &(*this)[indexOfPivot];
+    for(auto c = s.begin(); c != s.end(); c++) // omg c++
+        push_back(*c);
+    
+    pivot = &(*this)[indexOfPivot];
 }
 
 const int NumbleWord::sum()
 {
-	if(sum >= 0)
-		return sum;
-	else
-		sum = 0;
-		
-	for(auto i = begin(); i != end(); i++)
-		sum += *i;
-	return sum;
+    if(sum >= 0)
+        return sum;
+    else
+        sum = 0;
+        
+    for(auto i = begin(); i != end(); i++)
+        sum += *i;
+    return sum;
 }
 
 vector<int> * const NumbleWord::operator-> ()
 {
-	sum = -1;
-	sort(begin(), end());
-	return this;
+    sum = -1;
+    sort(begin(), end());
+    return this;
 }
 
 int * const NumbleWord::getPivot()
 {
-	return find(begin(), end(), pivot);
+    return find(begin(), end(), pivot);
 }
 
 const NumbleWord makeWord(const NumbleWord &dict, const int n)
 {
-	NumbleWord ret;
+    NumbleWord ret;
 }
--- Numble.h
+++ Numble.h
@@ -8,21 +8,21 @@ class NumbleWord: private vector<int>
 // represents a word in Numble, keeps sorted, maintains sum, maintains pivot
 {
 private:
-	int pivot;
-	mutable int sum;
-	
+    int pivot;
+    mutable int sum;
+    
 public:
-	NumbleWord(): pivot(0), sum(-1) {};
-	NumbleWord(const int n): vector<int>(n), pivot(0), sum(-1) {};
-	NumbleWord(const string& s, const int indexOfPivot);
-	NumbleWord(const vector<int>& v, const int p): vector<int>(v), pivot(p) {};
-	
+    NumbleWord(): pivot(0), sum(-1) {};
+    NumbleWord(const int n): vector<int>(n), pivot(0), sum(-1) {};
+    NumbleWord(const string& s, const int indexOfPivot);
+    NumbleWord(const vector<int>& v, const int p): vector<int>(v), pivot(p) {};
+    
 public:
-	const int sum() const;
-	const int &pivot() const {return pivot;};
-	void pivot(const int p) {pivot = p;};
-	int * const getPivot();
-	vector<int>* const operator-> ();  // monitors mutation of its inherited member
+    const int sum() const;
+    const int &pivot() const {return pivot;};
+    void pivot(const int p) {pivot = p;};
+    int * const getPivot();
+    vector<int>* const operator-> ();  // monitors mutation of its inherited member
 };
 
 const NumbleWord makeWord(const NumbleWord& dict, const int n=-1);  // creates word of size n from dict
