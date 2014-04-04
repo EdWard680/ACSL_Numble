c6ee28ad7517f420716000787dd938ef8c0c5613
EdWard680 <ezeward4@gmail.com>
2014-04-04 15:50:48 -0400

    Tests sized words

--- ACSL_Numble.cpp
+++ ACSL_Numble.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char **argv)
 {
-	for(int i = 1; i < argc; i++)
+	for(int i = 1; 2*i < argc; i++)
 	{
-		cout<<NumbleWord(argv[i])<<endl;
+		cout<<getSizedWord(NumbleWord(argv[2*i-1]), atoi(argv[2*i]))<<endl;
 	}
 	
 	return 0;
